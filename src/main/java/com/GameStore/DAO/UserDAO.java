4d590e4ce72538d2cc8b17a7395a5fa6ceb57e8c
Marcos-Henrique710 <marcoshenriqueufrn@gmail.com>
2025-12-29 00:39:41 -0300

    Atualizando o DAO do user

--- src/main/java/com/GameStore/DAO/UserDAO.java
+++ src/main/java/com/GameStore/DAO/UserDAO.java
@@ -1,6 +1,7 @@
 package com.GameStore.DAO;
 
 import com.GameStore.DataBase.ConexaoDB;
+import com.GameStore.model.Games;
 import com.GameStore.model.User;
 import java.sql.*;
 import java.util.ArrayList;
@@ -16,7 +17,7 @@ public class UserDAO {
     }
 
     private void inserir(User user) throws SQLException {
-        String sql = "INSERT INTO user (name, email, password, phone, adress, cpf) VALUES (?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO user (name, email, password, phone, address, cpf) VALUES (?, ?, ?, ?, ?, ?)";
 
         try (PreparedStatement ps = ConexaoDB.getInstance()
                 .getConnection().prepareStatement(sql)) {
@@ -31,4 +32,83 @@ public class UserDAO {
             ps.executeUpdate();
         }
     }
+
+    private void atualizar(User user) throws SQLException {
+        String sql = "UPDATE user SET name = ?, email = ?, password = ?, phone = ?, address = ?, cpf = ? WHERE id = ?";
+
+        try (PreparedStatement ps = ConexaoDB.getInstance()
+                .getConnection().prepareStatement(sql)) {
+
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassword());
+            ps.setString(4, user.getPhone());
+            ps.setString(5, user.getAddress());
+            ps.setString(6, user.getCpf());
+
+            ps.executeUpdate();
+        }
+    }
+
+    public void deletar(Integer id) throws SQLException {
+        String sql = "DELETE FROM user WHERE id = ?";
+
+        try (PreparedStatement ps = ConexaoDB.getInstance()
+                .getConnection().prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        }
+    }
+
+    public List<User> listar() throws SQLException {
+        List<Games> lista = new ArrayList<>();
+        String sql = "SELECT id, name, email, password, phone, address, cpf FROM user";
+
+        try (Statement st = ConexaoDB.getInstance().getConnection().createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                user.setPassword(rs.getString("password"));
+                user.setPhone(rs.getString("phone"));
+                user.setAddress(rs.getString("address"));
+                user.setCpf(rs.getString("cpf"));
+
+                lista.add(user);
+            }
+        }
+        return lista;
+    }
+
+    public List<User> buscarPorNome(String nome) throws SQLException {
+        List<User> lista = new ArrayList<>();
+        String sql = "SELECT id, name, email, password, phone, address, cpf FROM games WHERE name LIKE ?";
+
+        try (PreparedStatement ps = ConexaoDB.getInstance()
+                .getConnection().prepareStatement(sql)) {
+
+            ps.setString(1, "%" + nome + "%");
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    User user = new User();
+                    user.setId(rs.getInt("id"));
+                    user.setName(rs.getString("name"));
+                    user.setEmail(rs.getString("email"));
+                    user.setPassword(rs.getString("password"));
+                    user.setPhone(rs.getString("phone"));
+                    user.setAddress(rs.getString("address"));
+                    user.setCpf(rs.getString("cpf"));
+
+                    lista.add(user);
+                }
+            }
+        }
+        return lista;
+    }
+
 }
